a722660136155ed957121444aea2404f9690b53c
jamesakanoa <jamesdlee77@gmail.com>
2025-01-03 22:44:31 -0500

    modified package-lock.json, package.json, svg/AIIcon.js, svg/DataAnalyticsIcon.js, svg/ProductManagementIcon.js, svg/index.js

---
+++ svg/AIIcon.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function AIIcon({ className }) {
+  return (
+    <svg
+      className={className}
+      width="44"
+      height="44"
+      viewBox="0 0 44 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {/* Paste the contents of ai.svg here */}
+      <path
+        d="M22 0C9.85 0 0 9.85 0 22C0 34.15 9.85 44 22 44C34.15 44 44 34.15 44 22C44 9.85 34.15 0 22 0ZM22 40C11.95 40 4 32.05 4 22C4 11.95 11.95 4 22 4C32.05 4 40 11.95 40 22C40 32.05 32.05 40 22 40Z"
+        fill="#091E42"
+      />
+      <path
+        d="M30 18H26V26H30V18ZM18 26H14V30H18V26ZM22 14H18V22H22V14ZM26 22H22V30H26V22Z"
+        fill="#36B37E"
+      />
+    </svg>
+  );
+}
--- svg/index.js
+++ svg/index.js
@@ -3,25 +3,27 @@ import GithubIcon from "./GithubIcon";
 import TwitterIcon from "./TwitterIcon";
 import LinkedinIcon from "./LinkedinIcon";
 import MediumIcon from "./MediumIcon";
-import AngleListIcon from "./AngleListIcon";
 import ScrollToBottomIcon from "./ScrollToBottomIcon";
 import OpenMenuIcon from "./OpenMenuIcon";
 import CloseMenuIcon from "./CloseMenuIcon";
-import LanguagesIcon from "./LanguagesIcon";
-import SkillsIcon from "./SkillsIcon";
-import FrameworksIcon from "./FrameworksIcon";
+import AIIcon from "./AIIcon";
+import DataAnalyticsIcon from "./DataAnalyticsIcon";
+import ProductManagementIcon from "./ProductManagementIcon";
+
+export { default as AIIcon } from './AIIcon';
+export { default as DataAnalyticsIcon } from './DataAnalyticsIcon';
+export { default as ProductManagementIcon } from './ProductManagementIcon';
 
 export {
   CloseIcon,
   GithubIcon,
   TwitterIcon,
   LinkedinIcon,
-  AngleListIcon,
   MediumIcon,
   ScrollToBottomIcon,
   OpenMenuIcon,
   CloseMenuIcon,
-  FrameworksIcon,
-  SkillsIcon,
-  LanguagesIcon,
+  ProductManagementIcon,
+  DataAnalyticsIcon,
+  AIIcon,
 };
