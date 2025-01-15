6fc1b39b5cb707f40504dabdedbc97d0c56c8e86
jamesakanoa <jamesdlee77@gmail.com>
2025-01-15 16:32:20 -0500

    Updates

--- components/Modal/utils.ts
+++
@@ -1,4 +0,0 @@
-import { Project } from "./types";
-
-export const getProjectImages = (project: Project | null) =>
-  project?.images || (project?.image ? [project.image] : undefined);
--- config/emailjs.ts
+++
@@ -1,5 +0,0 @@
-export const EMAILJS_CONFIG = {
-  SERVICE_ID: "service_0rbzypn", // Replace with your EmailJS service ID
-  TEMPLATE_ID: "template_9blq6jb", // Replace with your EmailJS template ID
-  PUBLIC_KEY: "tWGVFcU1ZzNucelkI", // Replace with your EmailJS public key
-};
