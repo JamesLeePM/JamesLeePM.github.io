5773a365f14108a05917da1ca16dc781d58db0ac
jamesakanoa <jamesdlee77@gmail.com>
2025-01-14 22:00:03 -0500

    Update next.config.js to set basePath and assetPrefix for GitHub Pages deployment based on NODE_ENV. This change ensures proper routing and asset handling in production.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: "",
-  assetPrefix: "",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/jamesakanoa.github.io" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/jamesakanoa.github.io" : "",
   trailingSlash: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
